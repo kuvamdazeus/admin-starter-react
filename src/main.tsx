dc33d403593dd0f056771a3330fa4143352fd617
Kuvam Bhardwaj <kuvamyash@gmail.com>
2023-04-25 17:31:49 +0530

    - position screen imports at top

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
+import Dashboard from "./screens/Dashboard";
+import Login from "./screens/Login";
+import ProductsPage from "./screens/Products";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import { LayoutProvider } from "./layout/context/layoutcontext";
-
 import "primeflex/primeflex.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "./styles/layout/layout.scss";
 import "./index.css";
-
-import Dashboard from "./screens/Dashboard";
-import Login from "./screens/Login";
-import ProductsPage from "./screens/Products";
 
 const router = createBrowserRouter(
   createRoutesFromElements(

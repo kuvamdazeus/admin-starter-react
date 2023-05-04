eb506bcf96811b88f7de92b94ffb9b9717bec695
Kuvam Bhardwaj <kuvamyash@gmail.com>
2023-05-04 18:06:42 +0530

    - sidebar crud items

--- src/layout/AppMenu.tsx
+++ src/layout/AppMenu.tsx
@@ -1,33 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
-
-import React, { useContext } from "react";
 import AppMenuitem from "./AppMenuitem";
-import { LayoutContext } from "./context/layoutcontext";
 import { MenuProvider } from "./context/menucontext";
-import { AppMenuItem } from "../types/types";
+import menuItems from "./items.json";
 
 const AppMenu = () => {
-  const { layoutConfig } = useContext(LayoutContext);
-
-  const model: AppMenuItem[] = [
-    {
-      label: "CRUD Screens",
-      items: [
-        /* SIDEBAR ITEMS */
-      ],
-    },
-  ];
-
   return (
     <MenuProvider>
       <ul className="layout-menu">
-        {model.map((item, i) => {
-          return !item?.seperator ? (
-            <AppMenuitem item={item} root={true} index={i} key={item.label} />
-          ) : (
-            <li className="menu-separator"></li>
-          );
-        })}
+        {menuItems.map((item, i) => (
+          <AppMenuitem item={item} root={true} index={i} key={item.label} />
+        ))}
       </ul>
     </MenuProvider>
   );

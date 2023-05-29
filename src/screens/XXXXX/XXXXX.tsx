c3314551585177fae326253e948ca1e2d34fac72
Kuvam Bhardwaj <kuvamyash@gmail.com>
2023-05-29 21:14:18 +0530

    - add template string for table columns

--- src/screens/XXXXX/XXXXX.tsx
+++ src/screens/XXXXX/XXXXX.tsx
@@ -1,15 +1,15 @@
-import React, {useEffect, useRef, useState} from "react";
-import {Button} from "primereact/button";
-import {Column} from "primereact/column";
-import {DataTable} from "primereact/datatable";
-import {Dialog} from "primereact/dialog";
-import {InputText} from "primereact/inputtext";
-import {Toast} from "primereact/toast";
+import React, { useEffect, useRef, useState } from "react";
+import { Button } from "primereact/button";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { Toast } from "primereact/toast";
 import Layout from "@/layout/layout";
-import {XXXXXType} from "@/types/xxxxx";
-import {useNavigate} from "react-router-dom";
-import {fetcher} from "@/usefetcher";
-import {ServerResponse} from "@/types/types";
+import { XXXXXType } from "@/types/xxxxx";
+import { useNavigate } from "react-router-dom";
+import { fetcher } from "@/usefetcher";
+import { ServerResponse } from "@/types/types";
 
 const XXXXX = () => {
   const navigate = useNavigate();
@@ -26,10 +26,10 @@ const XXXXX = () => {
   const toast = useRef<Toast>(null);
   const dt = useRef<DataTable<XXXXXType[]>>(null);
 
-  const {data: entities, refetchData: refetchEntities} =
+  const { data: entities, refetchData: refetchEntities } =
     fetcher.useGET<ServerResponse<XXXXXType[]>>("/xxxxx");
 
-  const {deleteData} = fetcher.useDELETE({
+  const { deleteData } = fetcher.useDELETE({
     onSuccess: () => refetchEntities(),
   });
 
@@ -114,7 +114,7 @@ const XXXXX = () => {
   const imageBodyTemplate = (rowData: XXXXXType, fieldName: string) => {
     return (
       <>
-        <img src={rowData[fieldName]} className="shadow-2" width="100" alt={fieldName}/>
+        <img src={rowData[fieldName]} className="shadow-2" width="100" alt={fieldName} />
       </>
     );
   };
@@ -129,7 +129,7 @@ const XXXXX = () => {
           className="mr-2"
           onClick={() => navigate("/xxxxx/edit/" + rowData.id)}
         />
-        <Button icon="pi pi-trash" rounded severity="warning" onClick={() => confirmDelete(rowData)}/>
+        <Button icon="pi pi-trash" rounded severity="warning" onClick={() => confirmDelete(rowData)} />
       </>
     );
   };
@@ -138,13 +138,13 @@ const XXXXX = () => {
   const header = (
     <div className="flex justify-content-between">
       <div>
-         <span className="p-input-icon-left mr-2">
-            <i className="pi pi-search"/>
-            <InputText
-              type="search"
-              onInput={(e) => setGlobalFilter(e.currentTarget.value)}
-              placeholder="Search..."
-            />
+        <span className="p-input-icon-left mr-2">
+          <i className="pi pi-search" />
+          <InputText
+            type="search"
+            onInput={(e) => setGlobalFilter(e.currentTarget.value)}
+            placeholder="Search..."
+          />
         </span>
         <Button
           label="Delete Selected"
@@ -162,9 +162,9 @@ const XXXXX = () => {
           className="mr-3"
           onClick={() => importCsvInputRef.current?.click()}
         />
-        <input ref={importCsvInputRef} onChange={onFileImport} className="hidden" type="file" accept=".csv"/>
+        <input ref={importCsvInputRef} onChange={onFileImport} className="hidden" type="file" accept=".csv" />
 
-        <Button label="Export" icon="pi pi-upload" severity="help" onClick={exportCSV}/>
+        <Button label="Export" icon="pi pi-upload" severity="help" onClick={exportCSV} />
         <Button
           label="New"
           icon="pi pi-plus"
@@ -174,19 +174,18 @@ const XXXXX = () => {
         />
       </div>
     </div>
-
   );
 
   const deleteEntityDialogFooter = (
     <>
-      <Button label="No" icon="pi pi-times" text onClick={hideDeleteEntityDialog}/>
-      <Button label="Yes" icon="pi pi-check" text onClick={deleteEntity}/>
+      <Button label="No" icon="pi pi-times" text onClick={hideDeleteEntityDialog} />
+      <Button label="Yes" icon="pi pi-check" text onClick={deleteEntity} />
     </>
   );
   const deleteEntitiesDialogFooter = (
     <>
-      <Button label="No" icon="pi pi-times" text onClick={hideDeleteEntitiesDialog}/>
-      <Button label="Yes" icon="pi pi-check" text onClick={deleteSelected}/>
+      <Button label="No" icon="pi pi-times" text onClick={hideDeleteEntitiesDialog} />
+      <Button label="Yes" icon="pi pi-check" text onClick={deleteSelected} />
     </>
   );
 
@@ -194,7 +193,7 @@ const XXXXX = () => {
     <div className="grid crud-demo">
       <div className="col-12">
         <div className="card">
-          <Toast ref={toast}/>
+          <Toast ref={toast} />
           <h4>Manage xxxxx</h4>
           <DataTable
             ref={dt}
@@ -213,34 +212,35 @@ const XXXXX = () => {
             header={header}
             responsiveLayout="scroll"
           >
-            <Column selectionMode="multiple" headerStyle={{width: "4rem"}}></Column>
-            <Column header="Action" body={actionBodyTemplate} headerStyle={{minWidth: "10rem"}}></Column>
+            <Column selectionMode="multiple" headerStyle={{ width: "4rem" }}></Column>
+            <Column header="Action" body={actionBodyTemplate} headerStyle={{ minWidth: "10rem" }}></Column>
+            {/*TABLE_COLUMNS*/}
           </DataTable>
 
           <Dialog
             visible={deleteEntityDialog}
-            style={{width: "450px"}}
+            style={{ width: "450px" }}
             header="Confirm"
             modal
             footer={deleteEntityDialogFooter}
             onHide={hideDeleteEntityDialog}
           >
             <div className="flex align-items-center justify-content-center">
-              <i className="pi pi-exclamation-triangle mr-3" style={{fontSize: "2rem"}}/>
+              <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: "2rem" }} />
               {entity && <span>Are you sure you want to delete?</span>}
             </div>
           </Dialog>
 
           <Dialog
             visible={deleteEntitiesDialog}
-            style={{width: "450px"}}
+            style={{ width: "450px" }}
             header="Confirm"
             modal
             footer={deleteEntitiesDialogFooter}
             onHide={hideDeleteEntitiesDialog}
           >
             <div className="flex align-items-center justify-content-center">
-              <i className="pi pi-exclamation-triangle mr-3" style={{fontSize: "2rem"}}/>
+              <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: "2rem" }} />
               {entity && <span>Are you sure you want to delete the selected xxxxx?</span>}
             </div>
           </Dialog>
@@ -253,7 +253,7 @@ const XXXXX = () => {
 export default function XXXXXPage() {
   return (
     <Layout>
-      <XXXXX/>
+      <XXXXX />
     </Layout>
   );
 }
